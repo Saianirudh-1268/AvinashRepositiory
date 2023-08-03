14f4d2aea1d8b74636b044c3306ae83ff780b817
Anirudh <saianirudh2000g@gmail.com>
2023-08-03 12:35:30 +0530

    First commit

---
+++ X.java
@@ -0,0 +1,16 @@
+package com.qualizeal.inheritance.program;
+
+public class X {
+	public int a=10;
+	public int b=20;
+	
+	public X() {
+		System.out.println("x()");
+	}
+	public void hi() {
+		System.out.println("hi from X");
+	}
+	public void hello() {
+		System.out.println("hello from X");
+	}
+}
---
+++ Y.java
@@ -0,0 +1,17 @@
+package com.qualizeal.inheritance.program;
+
+public class Y extends X {
+	public int c=30;
+	public int d=40;
+	
+	public Y() {
+		System.out.println("y()");
+	}
+	public void hi() {
+		System.out.println("hi from Y");
+	}
+	public void hey() {
+		System.out.println("hey from Y");
+	}
+
+}
