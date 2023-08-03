eb6aeab22cf511dfed042f9fb92311681d02dddd
Anirudh <saianirudh2000g@gmail.com>
2023-08-03 12:03:13 +0530

    First commit

---
+++ src/main/java/com/qualizeal/app/operation/App.java
@@ -0,0 +1,22 @@
+package com.qualizeal.app.operation;
+
+public class App {
+	public int a=20;
+	public int b=10;
+    public int add() {
+    	int result = a + b;
+    	return result;
+    }
+    public int sub() {
+    	int result = a - b;
+    	return result;
+    }
+    public int mul() {
+    	int result = a * b;
+    	return result;
+    }
+    public int div() {
+    	int result = a / b;
+    	return result;
+    }
+}
---
+++ src/main/java/com/qualizeal/app/program/MainProgram.java
@@ -0,0 +1,14 @@
+package com.qualizeal.app.program;
+
+import com.qualizeal.app.operation.App;
+
+public class MainProgram {
+	public static void main( String[] args ) {
+		App app = new App();
+		
+		app.add();
+		app.sub();
+		app.mul();
+		app.div();
+	}
+}
